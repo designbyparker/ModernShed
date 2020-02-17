804c6aa67e0c8f536316db9fcb9d853afe46857b
CaityHeath <caity.heath@gmail.com>
2020-02-17 13:38:27 -0800

    carousel build right directional

---
+++ public/images/carousel/imagedata.js
@@ -0,0 +1,16 @@
+const imageData = [
+  {
+    src: '/images/carousel/sf-studio.jpg',
+    alt: 'San Fransico Studio'
+  },
+  {
+    src: '/images/carousel/sd-studio.jpg',
+    alt: 'San Diego Studio'
+  },
+  {
+    src: '/images/carousel/home-office.jpg',
+    alt: 'Home Office'
+  }
+];
+
+export default imageData;
