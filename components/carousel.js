aa75f70a5e4ef7873da26aea700bbce82ceadf71
CaityHeath <caity.heath@gmail.com>
2020-02-18 13:17:37 -0800

    double cursor, logic iteration in place with bugs

--- components/carousel.js
+++ components/carousel.js
@@ -1,7 +1,9 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import imageData from '../public/images/carousel/imageData';
 
+
 function Carousel(){
+
   let imageSet = [];
   imageData.forEach(img => {
     let display = {};
@@ -12,21 +14,67 @@ function Carousel(){
 
   const [image, setImage] = useState(imageSet[0]);
   const [index, setIndex] = useState(0);
+  const [cursor, setCursor] = useState('left');
+  const [leftBound, setLeftBound] = useState();
+  const [rightBound, setRightBound] = useState();
+
+  useEffect(() => {
+    let curs = document.querySelector('#carousel');
+    let offset = curs.offsetWidth
+    
+    setBounds(offset)
+  }, []);
+
+
+  function setBounds(offset){
+    setLeftBound(Math.floor(offset/2));
+    setRightBound(Math.floor(offset/2) + 1);
+  }
+
+  function handleMouseMove(e){
+    e.preventDefault();
+    let cursorPos = e.nativeEvent.offsetX;
+    if(cursorPos <= leftBound){
+      return setCursor('left');
+    }else{
+      return setCursor('right');
+    }
+  }
 
   function handleClick(e){
      e.preventDefault();
-     if(index === imageSet.length -1){
-       setIndex(0);
+     console.log(cursor);
+     if(cursor === 'right'){
+
+      if(index === imageSet.length -1){
+        setIndex(0);
+        setImage(imageSet[index]);
+        console.log(index);
+      }else{
+       setIndex(index + 1);
        setImage(imageSet[index]);
-     }else{
-      setIndex(index + 1);
-      setImage(imageSet[index]);
+       console.log(index);
+      }
+
+     }else if(cursor === 'left'){
+       if(index > 0){
+        setIndex(index - 1);
+        setImage(imageSet[index]);
+        console.log(index);
+       }else if(index === 0){
+         setIndex(imageSet.length - 1);
+         setImage(imageSet[index]);
+         console.log(index);
+
+       }
      }
     }
 
+
+
   return(
-    <section id="carousel">
-      <img src={image.src} alt={image.alt} className="carousel-image" onClick={handleClick}/>
+    <section id="carousel" className={cursor} onMouseMove={handleMouseMove}>
+      <img src={image.src} alt={image.alt} className="carousel-image" onClick={handleClick} draggable="false"/>
     </section>
   )
 }
