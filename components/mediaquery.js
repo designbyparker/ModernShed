08aee5ece94ccbee3682cdcdbd222cfe9de843bf
CaityHeath <caity.heath@gmail.com>
2020-02-21 13:00:29 -0800

    media queries working with window adjust

--- components/mediaquery.js
+++ components/mediaquery.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import { useEffect, useState } from "react";
+const [width, setWidth] = useState();
+useEffect(() => {
+  setWidth(window.innerWidth);
+  window.addEventListener('resize', updateWindowSize);
+}, []);
 
-const getClientWidth = () => {
-  const [width, setWidth] = useState();
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-  return width;
-}
 
-const renderMax = (media, children = null) => {
-  let width = getClientWidth();
-  console.log(width, 'from render max');
+const renderMax = (media, width, children = null) => {
   return media >= width ? children : null; 
 }
 
-const renderMin = (media, children = null) => {
-  let width = getClientWidth();
+const renderMin = (media, width, children = null) => {
   return media <= width ? children : null; 
 }
 
-export const Max = props => renderMax(props.media, props.children); 
-export const Min = props => renderMin(props.media, props.children);
+export const Max = props => renderMax(props.media, props.width,  props.children); 
+export const Min = props => renderMin(props.media, props.width, props.children);
--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,15 @@ import React from 'react';
 import Nav from '../components/nav';
 import DesktopCarousel from '../components/carousel';
 import MobileCarousel from '../components/mobileCarousel';
-import { Max, Min } from '../components/mediaquery';
 import '../styles/theme.css';
 
 function Index(){
+
   return(
     <div>
       <Nav/>
-      <Min media={600}>
-        <DesktopCarousel />
-      </Min>
-     
-      <Max media={599}>
-        <MobileCarousel/>
-      </Max>
+      <DesktopCarousel className="desktop-query"/>
+      <MobileCarousel className="mobile-query"/>
     </div>
   );
 }
