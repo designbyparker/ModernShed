60b2ca95315d370189c1d2d44d33aee5c1a8a6d1
CaityHeath <caity.heath@gmail.com>
2020-02-24 14:49:15 -0800

    tailored not configured

--- components/tailored.js
+++ components/tailored.js
@@ -2,13 +2,17 @@ const Tailored = () => {
   return(
     <section id="tailored-section" >
       <img src="https://via.placeholder.com/762x531" alt="Tailored not configured"/>
+
       <div id="tailored-right-column">
         <div className="headline-container">
           <h2>Tailored...</h2>
           <h2>not Configured.</h2>
+    
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </p>
         </div>  
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. </p>
-        <a href="/sheds">Sheds</a>
+        <div className="button-container">
+          <a href="/sheds">Sheds</a>
+        </div>
       </div>
     </section>
   )
