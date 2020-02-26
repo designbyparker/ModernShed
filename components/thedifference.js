4b00079bf42f917653a2a164d8e1a72418bef05d
CaityHeath <caity.heath@gmail.com>
2020-02-26 13:46:19 -0800

    typography updates

--- components/thedifference.js
+++ components/thedifference.js
@@ -4,7 +4,7 @@ const TheDifference = () => {
      <section id="the-difference-section">
        <div id="col-1">
           <h2>The Difference.</h2>
-          <p>(A) Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. (B) Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna </p>
+          <p><b>(A)</b> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. <b>(B)</b> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna </p>
           <div className="button-container"><a href='/process'>Process→</a></div>
        </div>
 
@@ -19,8 +19,8 @@ const TheDifference = () => {
       <div id="col-3">
         <div className="diff-image-container">
           <img src="https://via.placeholder.com/375x375" alt="some text about image" />
+          <h5>(B)</h5>
         </div>
-        <h5>(B)</h5>
       </div>
 
      </section>
