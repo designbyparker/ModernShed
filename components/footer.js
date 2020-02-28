5869e01893a28d585ed243f030f6f29fdf1f5c4c
CaityHeath <caity.heath@gmail.com>
2020-02-27 16:50:14 -0800

    type update to the image carousel and footer styling

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
     <section id="footer">
       <div className="footer-col-1">
         <img src="/branding/small-wordmark-white.svg" alt="Modern Shed Wordmark"/>
-        <p>Copy Rights</p>
+        <p>(c) Modern Shed, LLC All Rights Reserved. <br/>Privacy Policy. Terms of Use.</p>
       </div>
       <div className="footer-col-2 small-footer-col">
         <p className="section-header">SITE</p>
@@ -18,7 +18,7 @@ const Footer = () => {
       <div className="footer-col-3 small-footer-col">
         <p className="section-header">CONTENT</p>
         <div>
-          <a href='https://modernshedblog.com/'>Blog</a>
+          <a href='https://modernshedblog.com/' target="_blank">Blog</a>
           <a href='/legal'>Legal</a>
           <a href='/faq'>Faq</a>
           <a href='/press'>Press</a>
@@ -28,8 +28,10 @@ const Footer = () => {
       <div className="footer-col-4 small-footer-col">
         <p className="section-header">FOLLOW</p>
         <div>
-          <a href="https://www.instagram.com/modern_shed/">Instagram</a>
-          <a href="https://www.facebook.com/ModernShedInc/">Facebook</a>
+          <a href="https://www.instagram.com/modern_shed/" target="_blank">Instagram</a>
+          <a href="https://twitter.com/YourModernShed" target="_blank">Twitter</a>
+          <a href="https://www.facebook.com/ModernShedInc/" target="_blank">Facebook</a>
+          <a href="https://www.linkedin.com/company/modern-shed/" target="_blank">LinkedIn</a>
         </div>  
       </div>
 
