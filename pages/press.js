9bfd8c8fcdae2228f3b0b7e5a45655d4b5f8709c
CaityHeath <caity.heath@gmail.com>
2020-03-18 11:09:01 -0700

    aux pages styling and layout

--- pages/press.js
+++ pages/press.js
@@ -1,7 +1,20 @@
+import Footer from '../components/global/footer';
+import Nav from '../components/global/hamburger-nav';
+import '../styles/theme.css';
+
+
+
 const Press = () => {
   return(
-    <>
-    </>
+    <section id="press-page" className="aux-page">
+    <h1>LEGAL</h1>
+    <Nav/>
+    <section className="aux-page">
+      <div className="aux-deck">
+      </div>
+    </section>
+  <Footer/>
+  </section>
   );
 }
 
