c956e91dd1599b3b89ef719bff87448b027f671a
CaityHeath <caity.heath@gmail.com>
2020-03-18 09:36:08 -0700

    styling of dynamic components

--- components/home/featured-home.js
+++ components/home/featured-home.js
@@ -7,11 +7,13 @@ const FeaturedHome = (props) => {
     <section id="featured-home-section">
       <h2>Featured Sheds</h2>
 
-      <section id="featured-homes">
+      <section id="featured-home-deck">
         {props.featured.map(shed => {
           return(
-            <div id="shed.content_id">
-              <img src={shed.photos[0].url} alt={shed.photos[0].alt}/>
+            <div className="shed-card" id="shed.content_id">
+              <div className="image-container">
+                <img src={shed.photos[0].url} alt={shed.photos[0].alt}/>
+              </div>  
               <div className="shed-details-container">
                 <p>{shed.photos[0].description1}</p>
                 <p>{shed.photos[0].description2}</p>
