d6ceb43793f329b213b1fdc4ad50c0a53b17cb23
CaityHeath <caity.heath@gmail.com>
2020-03-23 16:33:21 -0700

    what we do section styled

--- components/home/what-we-do.js
+++ components/home/what-we-do.js
@@ -11,17 +11,17 @@ const WhatWeDo = () => {
       </div>
 
 
-      <div id="index-2-text-container">
-          <div>
+
+          <div id="whatwedo-headlines"> 
             <h2>There’s a type of shed for every purpose.</h2>
             <h3>You imagine, we’ll take care of the construction. We build intentionally crafted spaces for multi-functional use, by connecting the dots between form and function. Each room is tailored just for you.</h3>
           </div>
        
-          <div className="whatwedo-desc" >
+          <div id="whatwedo-desc" >
               <p>We could all use a little more room to grow. Modern-Shed was born from both builders and dreamers—idealists and pragmatic problem-solvers. Our answer to living in a limited space lies a few steps outside your door. Where could you use a little more room? A home office tucked back by the garden, a guest room decorated with sunlight, a studio meant for photography, writing, painting, or any hobby that doesn’t quite seem to fit across the kitchen table? Rather than a costly renovation, Modern-Shed wants to help you build the ideal outdoor space. Whether it’s a home office, an exercise room, a craft space, a music studio, a hobby room, a writer’s den, a meditation studio, or a play room for the kids—let’s lean into endless possibility.
               </p>
           </div>
-      </div> 
+
     </section>
 
   )
