8d6cec3d77f104890a4ba34f6c791d1f0712349b
CaityHeath <caity.heath@gmail.com>
2020-03-24 09:47:40 -0700

    sheds images and first two sections mobile styling

--- pages/sheds.js
+++ pages/sheds.js
@@ -1,11 +1,14 @@
 import ShedsHero from '../components/global/page-hero';
 import ContactCTA from '../components/global/contact-cta';
+import MobileContactCTA from '../components/global/mobile-contact-cta'; 
 import HamburgerNav from '../components/global/hamburger-nav';
 import Uses from '../components/sheds/uses';
+import MobileUses from '../components/sheds/mobile-uses';
 import Sizes from '../components/sheds/sizes';
 import Materials from '../components/sheds/materials';
 import Doors from '../components/sheds/doors';
 import Newsletter from '../components/global/newsletter';
+import MobileFooter from '../components/global/mobile-footer'
 import Footer from '../components/global/footer';
 import Head from 'next/head'
 import '../styles/theme.css';
@@ -19,12 +22,15 @@ const Sheds = () => {
       <HamburgerNav />
       <ShedsHero id="sheds-hero" page="SHEDS" copy="Let’s revise your living space with abundance in mind. "/>
       <Uses/>
-      <Sizes />
+      <MobileUses/>
+      {/* <Sizes />
       <ContactCTA buttontext="Inquire →"/>
+      <MobileContactCTA buttontext="Inquire →"/>
       <Materials/>
       <Doors/>
       <Newsletter/>
-      <Footer/>
+      <MobileFooter/>
+      <Footer/> */}
     </section>
   )
 }
